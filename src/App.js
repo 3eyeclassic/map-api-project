5bd41298b43af4f31b4d0fe69ae3fa8a4686f396
3eyeclassic <r.tr909@icloud.com>
2023-12-11 21:47:11 +0900

    初回コミット

--- src/App.js
+++ src/App.js
@@ -3,11 +3,7 @@ import Map from './components/map/Map';
 
 function App() {
   return (
-    <>
-      
-      
-      <h1>React & Google Map</h1>
-      
+    <>      
       <Map />
     </>
   );
--- src/components/map/Map.jsx
+++ src/components/map/Map.jsx
@@ -1,32 +1,77 @@
-import { GoogleMap, LoadScript, MarkerF } from '@react-google-maps/api';
-import React, { useState, useEffect } from 'react'
-import SearchBox from './SearchBox ';
+import React, { useRef } from 'react';
+import { GoogleMap, LoadScript, MarkerF, StandaloneSearchBox } from '@react-google-maps/api';
+
+const container = {
+  width: "75%",
+  height: "500px"
+};
+
+const position = {
+  lat: 35.680959106959,
+  lng: 139.76730676352
+};
 
 function Map() {
-  const container = {
-    width: "100%",
-    height: "100vh"
+  const searchBoxRef = useRef(null);
+
+  const handleLoad = (searchBox) => {
+    searchBoxRef.current = searchBox;
   };
-  
-  const position = {
-    lat: 35.680959106959,
-    lng: 139.76730676352
+
+  const handlePlacesChanged = () => {
+    if (searchBoxRef.current) {
+      const places = searchBoxRef.current.getPlaces();
+      if (places.length > 0) {
+        const newCenter = {
+          lat: places[0].geometry.location.lat(),
+          lng: places[0].geometry.location.lng()
+        };
+        // Update the map center to the selected place
+        // Optionally, you may want to update other components or state based on the selected place
+      }
+    }
   };
 
   return (
-    <LoadScript googleMapsApiKey='AIzaSyBs96NATLrbNmc7DC_RVO67j_rPj3QpdzY'>
-          <SearchBox />
+    <>
+      <h1>React & Google Map</h1>
+      <div className='wrap'>
+        <LoadScript googleMapsApiKey='AIzaSyDdBqvRgX8FtQI1CSFntBAnjWYW3tXUnyc'
+                    libraries={['places']}>
           <GoogleMap mapContainerStyle={container} center={position} zoom={15}>
-            {/*ここにmap上に表示する機能を記述  */}
-            {/* マーカー */}
+            {/* StandaloneSearchBox */}
+            <StandaloneSearchBox
+              onLoad={handleLoad}
+              onPlacesChanged={handlePlacesChanged}
+            >
+              <input
+                type="text"
+                placeholder="Search for a place"
+                style={{
+                  boxSizing: `border-box`,
+                  border: `1px solid transparent`,
+                  width: `240px`,
+                  height: `32px`,
+                  padding: `0 12px`,
+                  borderRadius: `3px`,
+                  boxShadow: `0 2px 6px rgba(0, 0, 0, 0.3)`,
+                  fontSize: `14px`,
+                  outline: `none`,
+                  textOverflow: `ellipses`,
+                  position: "absolute",
+                  left: "50%",
+                  marginLeft: "-120px"
+                }}
+              />
+            </StandaloneSearchBox>
+
+            {/* Marker */}
             <MarkerF position={position} label={"東京駅です！"} />
-            {/* サーチボックス */}
-          </GoogleMap>    
+          </GoogleMap>
         </LoadScript>
-  )
-};
-
-
-
+      </div>
+    </>
+  );
+}
 
 export default Map;
